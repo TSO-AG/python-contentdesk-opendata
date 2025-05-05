966692ba604049fca2fef836dbc83e2fa5cec029
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-05-05 14:22:23 +0200

    Bump version to 0.0.24 in pyproject.toml; refactor Load class to fix projectPath handling and improve folder path management

--- src/contentdeskopendata/load/load.py
+++ src/contentdeskopendata/load/load.py
@@ -8,8 +8,7 @@ class Load:
         self.path = path
         self.transformProducts = transformProducts
         self.loadProducts = self.setLoadProducts()
-        self.projetctPath = os.path.dirname(os.path.abspath(__file__))
-        
+               
     def getLoadProducts(self):
         return self.transformProducts
     
@@ -37,24 +36,26 @@ class Load:
     def loadProductsToFile(self, products, fileName):        
         # Check if folder exists
         # TODO: Fix Folder Path by Settings
-        print("Folder Path: ", self.projetctPath+"/"+self.path+"/api/")
-        if not os.path.exists(self.projetctPath+"/"+self.path+"/api/"):
-            os.makedirs(self.projetctPath+"/"+self.path+"/api/")
+        projetctPath = os.path.dirname(os.path.abspath(__file__))
+        print("Folder Path: ", projetctPath+"/"+self.path+"/api/")
+        if not os.path.exists(projetctPath+"/"+self.path+"/api/"):
+            os.makedirs(projetctPath+"/"+self.path+"/api/")
         
-        with open(self.projetctPath+"/"+self.path+"/api/"+fileName+".json", "w") as file:
+        with open(projetctPath+"/"+self.path+"/api/"+fileName+".json", "w") as file:
             file.write(json.dumps(products))
         
-    def debugToFile(self, products, fileName, path):
+    def debugToFile(products, fileName, path):
          # get current date and time
         current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
         #print("Current date & time : ", current_datetime)
         
         # convert datetime obj to string
         str_current_datetime = str(current_datetime)
+        projetctPath = os.path.dirname(os.path.abspath(__file__))
         # Check if folder exists
-        print("Folder Path: ", self.projetctPath+"/"+path+"/debug/"+str_current_datetime)
-        if not os.path.exists(self.projetctPath+"/"+path+"/debug/"+str_current_datetime):
-            os.makedirs(self.projetctPath+"/"+path+"/debug/"+str_current_datetime+"/")
+        print("Folder Path: ", projetctPath+"/"+path+"/debug/"+str_current_datetime)
+        if not os.path.exists(projetctPath+"/"+path+"/debug/"+str_current_datetime):
+            os.makedirs(projetctPath+"/"+path+"/debug/"+str_current_datetime+"/")
         
-        with open(self.projetctPath+"/"+path+"/debug/"+str_current_datetime+"/"+fileName+".json", "w") as file:
+        with open(projetctPath+"/"+path+"/debug/"+str_current_datetime+"/"+fileName+".json", "w") as file:
             file.write(json.dumps(products))
