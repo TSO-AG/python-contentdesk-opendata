834445f8e4c484ed0ffb745877c9616af1186647
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-06-18 15:35:09 +0200

    Bump version to 0.0.89 in pyproject.toml; update description field formatting in Load class change log

--- src/contentdeskopendata/load/load.py
+++ src/contentdeskopendata/load/load.py
@@ -402,7 +402,7 @@ class Load:
             file.write("The change log of the API is available at [/CHANGELOG](/CHANGELOG).\n\n")
             
             file.write("### Change for description field\n\n")
-            file.write('The description field got an upgrade to a full wysiwyg field with the following allowed tags: \"<em><strong><code><ul><ol><li><dl><dt><dd><h2><h3><h4><h5><h6><img><h1><pre><p><a><table><caption></caption><tbody><thead><tfoot><th><td><tr><br>\".\n')
+            file.write("The description field got an upgrade to a full wysiwyg field with the following allowed tags: &lt;em&gt;&lt;strong&gt;&lt;code&gt;&lt;ul&gt;&lt;ol&gt;&lt;li&gt;&lt;dl&gt;&lt;dt&gt;&lt;dd&gt;&lt;h2&gt;&lt;h3&gt;&lt;h4&gt;&lt;h5&gt;&lt;h6&gt;&lt;img&gt;&lt;h1&gt;&lt;pre&gt;&lt;p&gt;&lt;a&gt;&lt;table&gt;&lt;caption&gt;&lt;/caption&gt;&lt;tbody&gt;&lt;thead&gt;&lt;tfoot&gt;&lt;th&gt;&lt;td&gt;&lt;tr&gt;&lt;br&gt;.\n")
             file.write("In Open Data V1 there were just <p> tags allowed.\n\n")
 
             file.write("### More possible values in the @type field\n\n")
