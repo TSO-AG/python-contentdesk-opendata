ad4939fdb6cd56c0599c1b10248b854507c81d75
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-09-01 15:25:48 +0200

    Bump version to 0.0.128 in pyproject.toml; refactor GeoJsonTransformer to accept products as a parameter and update loadProductsToFile to pass products correctly

--- src/contentdeskopendata/load/geojson.py
+++ src/contentdeskopendata/load/geojson.py
@@ -1,6 +1,6 @@
 class GeoJsonTransformer:
-    def __init__(self, products):
-        self.products = products
+    def __init__(self):
+        pass
 
     def setGeoJsonData(self, product):
         geojson_feature = {
@@ -29,13 +29,14 @@ class GeoJsonTransformer:
             }
         return geojson_feature
 
-    def setProductsToGeoJson(self):
+    def setProductsToGeoJson(self, products):
         # Transform the GeoJSON data as needed
         geojson_data = {
             'type': 'FeatureCollection',
             'features': []
         }   
-        for product in self.products:
+        for product in products:
             geojson_feature = self.setGeoJsonData(product)
             geojson_data["features"].append(geojson_feature)
+            
         return geojson_data
