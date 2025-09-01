e0e6082ef3fc46a00a92312290b961150021071f
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-09-01 13:03:36 +0200

    Bump version to 0.0.113 in pyproject.toml; update GeoJsonTransformer method to setProductsToGeoJson in load.py

--- src/contentdeskopendata/load/load.py
+++ src/contentdeskopendata/load/load.py
@@ -196,7 +196,7 @@ class Load:
         if not os.path.exists(self.projectPath+"/api/"):
             os.makedirs(self.projectPath+"/api/")
 
-        geojson = GeoJsonTransformer(products).transform()
+        geojson = GeoJsonTransformer(products).setProductsToGeoJson()
         with open(self.projectPath+"/api/"+fileName+".geojson", "w", encoding="utf-8") as file:
             file.write(geojson)
 
