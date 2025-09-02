84a7e8ab608f404a8a999d07b89f1bdb7e21fefa
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-09-02 09:16:02 +0200

    Bump version to 0.0.133 in pyproject.toml; update mapCreator to streamline map content handling

--- src/contentdeskopendata/load/map.py
+++ src/contentdeskopendata/load/map.py
@@ -13,17 +13,14 @@ class mapCreator:
         #map = os.path.join(self.projectPath, "map/products.html")
         map = mapHtml.get_map_html()
 
-        with open(output_path, 'w', encoding='utf-8') as f:
-            f.write(map)
-
         # Replace the <title> tag content
-        content = content.replace('<title> Alle Produkte </title>', f'<title>{self.name}</title>')
+        map = map.replace('<title> Alle Produkte </title>', f'<title>{self.name}</title>')
         # Replace the geojson filename in the script tag
-        content = content.replace(
+        map = map.replace(
             "var geojsonURL = '/api/products.geojson'",
             f"var geojsonURL = '/api/{self.name}.geojson'"
         )
 
         # Write the modified content back
         with open(output_path, 'w', encoding='utf-8') as f:
-            f.write(content)
+            f.write(map)
